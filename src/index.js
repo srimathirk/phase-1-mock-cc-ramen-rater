953adcb4ad98f9b54010e8007b5cb90e0fd0b70f
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-04-29 11:30:24 -0400

    updating new ramen in lab

--- src/index.js
+++ src/index.js
@@ -1,24 +1,53 @@
 // write your code here
 
+//const ramenMenu = document.querySelector('#ramen-menu')
 document.addEventListener('DOMContentLoaded',() => {
-    fetch('http://localhost:3000/ramens').then(res => res.json()).then(ramens => { ramens.forEach(ramen=> createRamenMenuImg(ramen))});
+
+    fetch('http://localhost:3000/ramens').then(res => res.json()).then(ramens => { ramens.forEach(ramen=> createRamenImgMenu(ramen))})
 });
-function createRamenMenuImg(ramen){
-    const menuImg = document.createElement('img');
-    menuImg.src = ramen.image;
+function createRamenImgMenu(ramen){
+    const imgMenu = document.createElement('img');
+    imgMenu.src = ramen.image;
     //if click img see all menu info
-    menuImg.addEventListener('click',()=>{ 
+    imgMenu.addEventListener('click',()=>{ 
+       // e.preventDefault();
         const imgDetails = document.querySelector('#ramen-detail .detail-image');
-        imgDetails.src = 'ramen.image';
+        imgDetails.src = ramen.image;
         const nameDetails = document.querySelector('#ramen-detail .name');
-        nameDetails.textContent = 'ramen.name';
+        nameDetails.textContent = ramen.name;
         const restaurantDetails = document.querySelector('#ramen-detail .restaurant');
-        restaurantDetails.textContent = 'ramen.restaurant';
+        restaurantDetails.textContent = ramen.restaurant;
         const ratingDetails = document.querySelector('#rating-display');
-        ratingDetails.textContent = 'ramen.rating';
+        ratingDetails.textContent = ramen.rating;
         const commentDetails = document.querySelector('#comment-display');
-        commentDetails.textContent = 'ramen.comment';
+        commentDetails.textContent = ramen.comment;
 });
-const ramenMenu = document.querySelector('#ramen-menu')
-ramenMenu.append(menuImg);
+//div#ramen-menu.append(imgDetails ,nameDetails ,restaurantDetails, ratingDetails , commentDetails)
+document.getElementById('ramen-menu').append(imgMenu)
+   }
+//adding new ramen
+const form = document.getElementById('new-ramen')
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const formData = Object.fromEntries(new FormData(e.target))
+    console.log(formData);
+    sendPost(formData)
+})
+
+function sendPost(newRamen){
+    fetch('http://localhost:3000/ramens',{
+        method: "POST",
+        headers: 
+        {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            /* name = newRamen.name , restaurant = newRamen.restaurant,
+               image = newRamen.image, rating = newRamen.rating, comment = newRamen.comment*/
+               ...newRamen,
+        })
+    }).then(res=>res.json()).then(newRamen =>createRamenImgMenu(newRamen))
 }
+
+
